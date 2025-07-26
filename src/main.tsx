39d3400351bb745b0e38bbcb1f77f74591fa606f
urchifox <126313096+urchifox@users.noreply.github.com>
2025-07-26 17:59:35 +0400

    remove all default data

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import "./index.css"
+import "./main.scss"
 
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-import App from "./App.tsx"
+import { App } from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
