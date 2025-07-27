9e1dca9c01d3a36d697d31ca746f804c34915158
urchifox <126313096+urchifox@users.noreply.github.com>
2025-07-27 17:36:52 +0400

    correct level setting

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { type LevelConfig, createLevel } from "./game"
 
 export function App() {
 	const levelConfig: LevelConfig = {
-		columns: 5,
+		columns: 3,
 		rows: 3,
-		shuffleSteps: { min: 2, max: 2 },
+		shuffleSteps: { min: 3, max: 10 },
 	}
 	const level = createLevel(levelConfig)
 
