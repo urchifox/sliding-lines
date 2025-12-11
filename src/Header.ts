b2a5b5eb6c22efe1afc7c039020694a387c7adc6
urchifox <126313096+urchifox@users.noreply.github.com>
2025-12-11 21:39:16 +0000

    correct setting margins in header and text components

--- src/Header.ts
+++ src/Header.ts
@@ -3,13 +3,13 @@ import styled from "@emotion/styled"
 import { theme } from "./assets/styles/theme"
 
 export const HeaderStyled = styled.h1<{
-	marginTop?: number
-	marginBottom?: number
+	marginTop?: string
+	marginBottom?: string
 }>(
 	({ marginTop, marginBottom }) => `
     margin-inline: 0;
-    margin-top: ${marginTop ? marginTop + "px" : "0"};
-    margin-bottom: ${marginBottom ? marginBottom + "px" : "2em"};
+    margin-top: ${marginTop ?? "0"};
+    margin-bottom: ${marginBottom ?? "2em"};
 	text-align: center;
 	color: ${theme.color.primary};
     font-size: ${theme.fontSize.xl};
