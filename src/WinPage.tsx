f3f2145577f531fc9bf8aad2282de89dc9bcf8be
Katherine Evtukhova <126313096+urchifox@users.noreply.github.com>
2025-12-12 02:42:29 +0400

    add puzze image as background to win page (#7)
    * add puzze image as bbackground to win page
    
    * format files

--- src/WinPage.tsx
+++ src/WinPage.tsx
@@ -1,21 +1,42 @@
+import styled from "@emotion/styled"
+import { useState } from "react"
+
 import { AppRoute } from "./App"
 import { Button } from "./Button"
 import { HeaderStyled } from "./Header"
 import { PageStyled } from "./Page"
+import { getLevelNumber } from "./game"
+import { type ImageInfo, getImageInfo } from "./images"
+
+const WinPageStyled = styled(PageStyled)<{ background: string }>(({
+	background,
+}) => {
+	return `
+		background-image: url(${background});
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: contain;
+	`
+})
 
 export function WinPage({
 	setPage,
 }: {
 	setPage: React.Dispatch<React.SetStateAction<AppRoute>>
 }) {
+	const [imageInfo, setImageInfo] = useState<ImageInfo | null>(null)
+	if (imageInfo === null) {
+		getImageInfo(getLevelNumber() - 1).then(setImageInfo)
+	}
+
 	const handleStartClick = () => {
 		setPage(AppRoute.Game)
 	}
 
 	return (
-		<PageStyled>
+		<WinPageStyled background={imageInfo?.imageUrl ?? ""}>
 			<HeaderStyled>You have won!</HeaderStyled>
 			<Button onClick={handleStartClick} text="Solve next puzzle" />
-		</PageStyled>
+		</WinPageStyled>
 	)
 }
